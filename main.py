57ee254c1bc0fcd1429789abbf773429f17423e2
pkrono <peter.rono87@gmail.com>
2023-05-05 05:10:18 +0300

    move items to csv file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+
 class Item:
     pay_rate = 0.8 # pay rate after 20% discount
-    
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
         
         #Run Validations
@@ -11,15 +13,26 @@ class Item:
         self.price = price
         self.quantity = quantity
         
+        #Actions to execute
+        Item.all.append(self)
+        
     def cal_total_price(self):
-        print(self.name)
         return self.price * self.quantity
     
-item1 = Item("phone", 100, 5)
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+    
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        
+        for item in items:
+            print(item)
+        
+        
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity}))"
 
-m = item1.cal_total_price()
-print(m)
-
-item2 = Item("Laptop", 1000, 3)
-
-print(item2.cal_total_price())
+Item.instantiate_from_csv()
