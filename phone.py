9c0807f2c7a8a1fe7f0af679fbf2a1cec25c9417
pkrono <peter.rono87@gmail.com>
2023-05-06 06:23:05 +0300

    move each class to its own file

---
+++ phone.py
@@ -0,0 +1,12 @@
+from item import Item
+
+# Inheritance
+class Phone(Item):
+    
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+        super().__init__(name, price, quantity)
+        
+        assert broken_phones >= 0, f"Broken Phones {broken_phones} is not greater than or equal to 0"
+        
+        self.broken_phones = broken_phones
+        
